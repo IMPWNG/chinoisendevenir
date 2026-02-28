b4e4aee9e230339ae04e0d49441230441aa711b7
IMPWNG <matisse.ach+github@gmail.com>
2026-02-28 21:37:49 +0700

    fixmodule

--- api/subscribe.js
+++ api/subscribe.js
@@ -1,6 +1,6 @@
 const https = require("https");
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
@@ -81,4 +81,4 @@ export default async function handler(req, res) {
     brevoReq.write(payload);
     brevoReq.end();
   });
-}
+};
