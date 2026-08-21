20616b461d9dd77a8e6a81576ad874edfa285b4f
IMPWNG <matisse.ach+github@gmail.com>
2026-08-21 17:11:24 +0800

    fix

--- src/lib/emailConfig.js
+++ src/lib/emailConfig.js
@@ -1,5 +1,8 @@
 export const CONTACT_FROM_EMAIL = "contact@chinoisendevenir.com";
 export const ADMIN_NOTIFY_EMAIL = "chinoisendevenir@gmail.com";
 
+export const CONTACT_FROM_EMAIL = "contact@chinoisendevenir.com";
+export const ADMIN_NOTIFY_EMAIL = "chinoisendevenir@gmail.com";
+
 export const INBOUND_REPLY_TO =
-  process.env.RESEND_INBOUND_EMAIL || "contact@inbound.chinoisendevenir.com";
+  process.env.RESEND_INBOUND_EMAIL || CONTACT_FROM_EMAIL;
